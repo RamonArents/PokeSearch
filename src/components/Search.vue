79bb694ce4ab636f3e0af06c87040070f2a45777
ramon arents <ramonarents@hotmail.com>
2022-11-05 18:00:38 +0100

    validation

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="md-5 position-absolute mt-25">
-    <!--TODO: Door de data heen loopen en de style netter maken-->
-    <p style="color:white">{{ pokemon }}</p>
-    <input v-model="pokemon" @keyup.enter="getPokemon()" type="search" placeholder="Search for Pokemon" />
+    <div v-if="isOk">
+      <p :name="pokemons" v-for="pokemon in pokemons">{{ pokemon.name }}</p>
+    </div>
+    <div v-else>
+      <p :name="pokemons">Sorry, this pokemon doesn't seem to exist.</p>
+    </div>
+    <input v-model="pokemons" @keyup.enter="getPokemon()" type="search" placeholder="Search for Pokemon" />
   </div>
 </template>
 
@@ -13,18 +17,21 @@ export default {
   name: "Search",
   data: function(){
     return {
-      pokemon: this.pokemon,
-      errorMessage: null
+      pokemons: this.pokemons,
+      errorMessage: null,
+      isOk: true
     };
   },
   methods:{
     async getPokemon(){
       try{
-        let response = await PokeService.getPokemon(this.pokemon);
-        this.pokemon = response.data;
+        let response = await PokeService.getPokemon(this.pokemons);
+        this.pokemons = response.data;
+        this.isOk = true;
       }
       catch(error){
         this.errorMessage = error;
+        this.isOk = false;
       }
     }
   },
@@ -32,6 +39,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+p{
+  color:white;
+}
 .mt-25{
   margin-top:25rem;
 }
